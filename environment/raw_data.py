efe71e420f8d30b97f9eba8f6c85d4fcad7cf267
Bryan Deng <bryan.deng002@gmail.com>
2025-01-27 19:48:10 -0500

    add rate limit bypass

--- environment/raw_data.py
+++ environment/raw_data.py
@@ -3,6 +3,8 @@ from twelvedata import TDClient
 import pandas_market_calendars as mcal
 import pandas as pd
 from datetime import timedelta
+from itertools import batched
+import time
 
 env_config = dotenv_values(".env")
 
@@ -10,12 +12,14 @@ td = TDClient(env_config["TWELVE_DATA_API_KEY"])
 nyse_cal = mcal.get_calendar("NYSE")
 
 
-def load_data(symbols: list[str], start_date, end_date):
+def _load_data_limited(symbols: tuple[str], start_date, end_date):
     """
     Get adjusted OHCLV data for symbols in open days across [start_date, end_date].
 
-    Uses 1 API token per symbol per call.
+    Uses 1 API token per symbol per call. Limited at 8 symbols per call (12data limit).
     """
+
+    assert len(symbols) <= 8
 
     # add 1 day to end_date to make the date range inclusive
     if isinstance(end_date, str):
@@ -40,20 +44,33 @@ def load_data(symbols: list[str], start_date, end_date):
                 "volume": "Volume",
             }
         )
-        .sort_values(by=["Date", "Id"])
     )
     ts["Date"] = pd.to_datetime(ts["Date"])
     ts = ts[ts["Date"].isin(open_days)]
-    ts.reset_index(drop=True, inplace=True)
 
     return ts
 
 
-# def test():
-#     data = load_data(
-#         ["AAPL", "MSFT", "TSLA", "VOD"], start_date="2020-01-01", end_date="2020-02-01"
-#     )
-#     print(data)
+def load_data(symbols: list[str], start_date, end_date, _batch_size=8):
+    _RATE_LIMIT = 60  # seconds between calls
+
+    ret = pd.DataFrame()
+    for i, symbol_batch in enumerate(batched(symbols, n=_batch_size)):
+        if i != 0:
+            # don't wait for rate limit on first call
+            time.sleep(_RATE_LIMIT)
+        batch_df = _load_data_limited(symbol_batch, start_date, end_date)
+        ret = pd.concat([ret, batch_df])
+
+    ret.sort_values(by=["Date", "Id"], inplace=True)
+    ret.reset_index(drop=True, inplace=True)
+    return ret
 
 
-# test()
+if __name__ == "__main__":
+    data = load_data(
+        ["AAPL", "MSFT", "TSLA", "VOD", "NVDA", "AMZN", "BA", "DELL", "INTC", "CRM"],
+        start_date="2020-01-01",
+        end_date="2020-02-01",
+    )
+    print(data)
